3eb0beb504ca5211a9a96af2c8120c77f6b37448
ERick9483 <erick_on94@hotmail.com>
2016-11-10 20:12:44 -0600

    Base

---
+++ Conteo.c
@@ -0,0 +1,26 @@
+/*COnteo de numeros pares en una serie de numeros
+utilizando un while*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+int num,pares=0;
+
+    puts("\n\t COnteo de numeros pares\n");
+    printf("\n\t Ingrese numero (fin con cero): ");
+    scanf("%d",&num);
+
+    while(num!=0){
+        if(num%2==0)
+        {
+            pares++;
+        }
+        printf("\n\t Igrese numero (fin con cero): ");
+        scanf("%d",&num);
+
+    }
+    printf("\n\t Se ingresaron %d pares\n",pares);
+
+    system("pause");
+}
